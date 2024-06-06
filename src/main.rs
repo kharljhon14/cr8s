aecb9da9176314bd1c40996475b68ab00ae4e383
kharle14 <enriquezkharl14@gmail.com>
2024-06-07 00:55:29 +0800

    [Feat] db connection

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use rocket_db_pools::{Connection, Database};
+
 mod models;
 mod respositories;
 mod schema;
 
+#[derive(Database)]
+#[database("postgres")]
+struct DbConnection(rocket_db_pools::diesel::PgPool);
+
 #[rocket::get("/rustaceans")]
-fn get_rustaceans() {}
+fn get_rustaceans(db: Connection<DbConnection>) {}
 
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
         .mount("/", rocket::routes![get_rustaceans])
+        .attach(DbConnection::init())
         .launch()
         .await;
 }
