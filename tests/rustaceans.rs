f0a88b26d15d9da463de5ae010afc4dcb89fa0ed
kharle14 <enriquezkharl14@gmail.com>
2024-06-07 23:38:58 +0800

    [Test] Create rustacean test"

--- tests/rustaceans.rs
+++ tests/rustaceans.rs
@@ -1,4 +1,5 @@
 use reqwest::{blocking::Client, StatusCode};
+use rocket::serde::json::{serde_json::json, Value};
 
 #[test]
 fn test_get_rustaceans() {
@@ -12,4 +13,28 @@ fn test_get_rustaceans() {
 }
 
 #[test]
-fn test_create_rustacean() {}
+fn test_create_rustacean() {
+    let client = Client::new();
+    let response = client
+        .post("http://127.0.0.1:8000/rustaceans")
+        .json(&json!({
+            "name": "Foo bar",
+            "email": "foobar@gmail.com"
+        }))
+        .send()
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    let rustacean: Value = response.json().unwrap();
+
+    assert_eq!(
+        rustacean,
+        json!({
+            "id": rustacean["id"],
+            "name": "Foo bar",
+            "email": "foobar@gmail.com",
+            "created_at": rustacean["created_at"],
+        })
+    );
+}
