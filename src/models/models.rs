2d4225e72a279daf26302aa8fefca74217feb19e
kharle14 <enriquezkharl14@gmail.com>
2024-06-07 12:25:30 +0800

    [Fix] Update Rustacean to skip id and created_at when deserializing

--- src/models/models.rs
+++ src/models/models.rs
@@ -5,9 +5,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Queryable, AsChangeset, Serialize, Deserialize)]
 pub struct Rustacean {
+    #[serde(skip_deserializing)] // Marking this as optional
     pub id: i32,
     pub name: String,
     pub email: String,
+    #[serde(skip_deserializing)]
     pub created_at: NaiveDateTime,
 }
 
