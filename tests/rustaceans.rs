bda61eb47d825490a225906acc3aa0e4a8efc0e2
kharle14 <enriquezkharl14@gmail.com>
2024-06-08 11:32:46 +0800

    [Test] Rustacean tesst

--- tests/rustaceans.rs
+++ tests/rustaceans.rs
@@ -38,3 +38,83 @@ fn test_create_rustacean() {
         })
     );
 }
+
+#[test]
+fn test_get_rustacean() {
+    let client = Client::new();
+    let response = client
+        .post("http://127.0.0.1:8000/rustaceans")
+        .json(&json!({
+            "name": "Foo bar",
+            "email": "foobar@gmail.com"
+        }))
+        .send()
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    let rustacean: Value = response.json().unwrap();
+
+    let response = client
+        .get(format!(
+            "http://127.0.0.1:8000/rustaceans/{}",
+            rustacean["id"]
+        ))
+        .send()
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    assert_eq!(
+        rustacean,
+        json!({
+            "id": rustacean["id"],
+            "name": "Foo bar",
+            "email": "foobar@gmail.com",
+            "created_at": rustacean["created_at"],
+        })
+    );
+}
+
+#[test]
+fn test_update_rustacean() {
+    let client = Client::new();
+    let response = client
+        .post("http://127.0.0.1:8000/rustaceans")
+        .json(&json!({
+            "name": "Foo bar",
+            "email": "foobar@gmail.com"
+        }))
+        .send()
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    let rustacean: Value = response.json().unwrap();
+
+    let response = client
+        .put(format!(
+            "http://127.0.0.1:8000/rustaceans/{}",
+            rustacean["id"]
+        ))
+        .json(&json!({
+            "name": "Foo BAR",
+            "email": "fooobar@mail.com"
+        }))
+        .send()
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let rustacean: Value = response.json().unwrap();
+
+    assert_eq!(
+        rustacean,
+        json!({
+            "id": rustacean["id"],
+            "name": "Foo BAR",
+            "email": "fooobar@mail.com",
+            "created_at": rustacean["created_at"],
+        })
+    );
+}
