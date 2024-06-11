c51488555df2e14b7d0b74fc68b46b18c572e7e0
kharle14 <enriquezkharl14@gmail.com>
2024-06-11 12:39:50 +0800

    [Test] Get crate test

--- tests/crates.rs
+++ tests/crates.rs
@@ -8,7 +8,6 @@ pub mod common;
 fn test_create_crate() {
     let client = Client::new();
     let rustacean = common::create_test_rustacean(&client);
-
     let response = client
         .post(format!("{}/crates", APP_HOST))
         .json(&json!({
@@ -19,7 +18,7 @@ fn test_create_crate() {
             "description": "Crate description"
         }))
         .send()
-        .unwrap();
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
 
     assert_eq!(response.status(), StatusCode::CREATED);
 
@@ -35,6 +34,39 @@ fn test_create_crate() {
             "description": "Crate description",
             "created_at": a_crate["created_at"]
         })
+    );
+
+    common::delete_test_crate(&client, a_crate);
+    common::delete_test_rustacean(&client, rustacean);
+}
+
+#[test]
+fn test_get_crate() {
+    let client = Client::new();
+    let rustacean = common::create_test_rustacean(&client);
+
+    let a_crate = common::create_test_crate(&client, &rustacean);
+
+    let response = client
+        .get(format!("{}/crates/{}", APP_HOST, a_crate["id"]))
+        .send()
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let a_crate: Value = response.json().unwrap();
+
+    assert_eq!(
+        a_crate,
+        json!(json!({
+            "id": a_crate["id"],
+            "rustaceans_id": rustacean["id"],
+            "code": "Foo",
+            "name": "Crate",
+            "version": "0.1",
+            "description": "Crate description",
+            "created_at": a_crate["created_at"]
+        }))
     );
 
     common::delete_test_crate(&client, a_crate);
