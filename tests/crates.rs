aaada0d8a1481d74bccbcfc1ab4f6b1df8248e29
kharle14 <enriquezkharl14@gmail.com>
2024-06-11 12:30:45 +0800

    [Test] Create crate test

--- tests/crates.rs
+++ tests/crates.rs
@@ -1,6 +1,42 @@
-use reqwest::blocking::Client;
+use common::APP_HOST;
+use reqwest::{blocking::Client, StatusCode};
+use serde_json::{json, Value};
+
+pub mod common;
 
 #[test]
 fn test_create_crate() {
     let client = Client::new();
+    let rustacean = common::create_test_rustacean(&client);
+
+    let response = client
+        .post(format!("{}/crates", APP_HOST))
+        .json(&json!({
+            "rustaceans_id": rustacean["id"],
+            "code": "Foo",
+            "name": "Crate",
+            "version": "0.1",
+            "description": "Crate description"
+        }))
+        .send()
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    let a_crate: Value = response.json().unwrap();
+    assert_eq!(
+        a_crate,
+        json!({
+            "id": a_crate["id"],
+            "rustaceans_id": rustacean["id"],
+            "code": "Foo",
+            "name": "Crate",
+            "version": "0.1",
+            "description": "Crate description",
+            "created_at": a_crate["created_at"]
+        })
+    );
+
+    common::delete_test_crate(&client, a_crate);
+    common::delete_test_rustacean(&client, rustacean);
 }
--- tests/rustaceans.rs
+++ tests/rustaceans.rs
@@ -1,13 +1,13 @@
-use common::{create_test_rustacean, delete_test_rustacean, APP_HOST};
+use common::APP_HOST;
 use reqwest::{blocking::Client, StatusCode};
 use serde_json::{json, Value};
 
-mod common;
+pub mod common;
 
 #[test]
 fn test_get_rustaceans() {
     let client = Client::new();
-    let rustacean = create_test_rustacean(&client);
+    let rustacean = common::create_test_rustacean(&client);
 
     let response = client
         .get(format!("{}/rustaceans", APP_HOST))
@@ -20,7 +20,7 @@ fn test_get_rustaceans() {
 
     assert!(json.as_array().unwrap().contains(&rustacean));
 
-    delete_test_rustacean(&client, rustacean)
+    common::delete_test_rustacean(&client, rustacean);
 }
 
 #[test]
@@ -48,13 +48,13 @@ fn test_create_rustacean() {
             "created_at": rustacean["created_at"],
         })
     );
-    delete_test_rustacean(&client, rustacean)
+    common::delete_test_rustacean(&client, rustacean);
 }
 
 #[test]
 fn test_get_rustacean() {
     let client = Client::new();
-    let rustacean = create_test_rustacean(&client);
+    let rustacean = common::create_test_rustacean(&client);
 
     let response = client
         .get(format!("{}/rustaceans/{}", APP_HOST, rustacean["id"]))
@@ -72,13 +72,13 @@ fn test_get_rustacean() {
             "created_at": rustacean["created_at"],
         })
     );
-    delete_test_rustacean(&client, rustacean)
+    common::delete_test_rustacean(&client, rustacean);
 }
 
 #[test]
 fn test_update_rustacean() {
     let client = Client::new();
-    let rustacean = create_test_rustacean(&client);
+    let rustacean = common::create_test_rustacean(&client);
 
     let response = client
         .put(format!("{}/rustaceans/{}", APP_HOST, rustacean["id"]))
@@ -102,13 +102,13 @@ fn test_update_rustacean() {
             "created_at": rustacean["created_at"],
         })
     );
-    delete_test_rustacean(&client, rustacean)
+    common::delete_test_rustacean(&client, rustacean);
 }
 
 #[test]
 fn test_delete_rustacean() {
     let client = Client::new();
-    let rustacean = create_test_rustacean(&client);
+    let rustacean = common::create_test_rustacean(&client);
 
     let response = client
         .delete(format!("{}/rustaceans/{}", APP_HOST, rustacean["id"]))
