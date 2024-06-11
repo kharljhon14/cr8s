c63d9f294720b84c9fe5638120c50e1ace2a9f5a
kharle14 <enriquezkharl14@gmail.com>
2024-06-11 11:35:42 +0800

    [Test] added common mod

---
+++ tests/common/mod.rs
@@ -0,0 +1,28 @@
+use reqwest::{blocking::Client, StatusCode};
+use serde_json::{json, Value};
+
+pub static APP_HOST: &'static str = "http://127.0.0.1:8000";
+
+pub fn create_test_rustacean(client: &Client) -> Value {
+    let response = client
+        .post(format!("{}/rustaceans", APP_HOST))
+        .json(&json!({
+            "name": "Foo bar",
+            "email": "foobar@gmail.com"
+        }))
+        .send()
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    response.json().unwrap()
+}
+
+pub fn delete_test_rustacean(client: &Client, rustacean: Value) {
+    let response = client
+        .delete(format!("{}/rustaceans/{}", APP_HOST, rustacean["id"]))
+        .send()
+        .unwrap_or_else(|err| panic!("Request failed {:?}", err));
+
+    assert_eq!(response.status(), StatusCode::NO_CONTENT);
+}
