9ffa8a2ba27a4da6935e738eda283bdb08bbc674
kharle14 <enriquezkharl14@gmail.com>
2024-06-12 20:34:52 +0800

    [Chore] added new file for models

--- src/models/crates.rs
+++ src/models/crates.rs
@@ -3,23 +3,6 @@ use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, AsChangeset, Serialize, Deserialize)]
-pub struct Rustacean {
-    #[serde(skip_deserializing)] // Marking this as optional
-    pub id: i32,
-    pub name: String,
-    pub email: String,
-    #[serde(skip_deserializing)]
-    pub created_at: NaiveDateTime,
-}
-
-#[derive(Insertable, Deserialize)]
-#[diesel(table_name=rustaceans)]
-pub struct NewRustacean {
-    pub name: String,
-    pub email: String,
-}
-
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct Crate {
     #[serde(skip_deserializing)]
