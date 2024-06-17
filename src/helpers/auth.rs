dbaf2a46d8f95f1b84f281ac9a2139817e63b045
kharle14 <enriquezkharl14@gmail.com>
2024-06-17 23:38:42 +0800

    [Chore] Moved hashing password to helper module

--- src/helpers/auth.rs
+++ src/helpers/auth.rs
@@ -1,4 +1,9 @@
-use argon2::{password_hash::Error, PasswordHash, PasswordVerifier};
+use argon2::{
+    password_hash::{rand_core::OsRng, Error, PasswordHasher, SaltString},
+    PasswordHash, PasswordVerifier,
+};
+
+use rand::{distributions::Alphanumeric, Rng};
 
 use crate::models::users::User;
 
@@ -13,7 +18,20 @@ pub fn authorize_user(user: &User, credentials: Credentials) -> Result<String, E
     let db_hash = PasswordHash::new(&user.password)?;
     argon2.verify_password(credentials.password.as_bytes(), &db_hash)?;
 
-    let session_id = String::from("");
+    let session_id = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(128)
+        .map(char::from)
+        .collect();
 
     Ok(session_id)
+}
+
+pub fn hash_password(password: String) -> Result<String, Error> {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = argon2::Argon2::default();
+
+    let hashed_password = argon2.hash_password(password.as_bytes(), &salt)?;
+
+    Ok(hashed_password.to_string())
 }
